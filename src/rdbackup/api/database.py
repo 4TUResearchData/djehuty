63cd9052c0066b045e59b89ad158560f048a1844
Roel Janssen <r.r.e.janssen@tudelft.nl>
2021-11-19 13:30:41 +0100

    api: database: Add initial collections fetch functionality.
    * src/rdbackup/api/database.py: Implement the “collections” procedure.

--- src/rdbackup/api/database.py
+++ src/rdbackup/api/database.py
@@ -645,3 +645,154 @@ LIMIT {limit}
             logging.error(f"Query:\n---\n{query}\n---")
 
         return results
+
+    ## ------------------------------------------------------------------------
+    ## COLLECTIONS
+    ## ------------------------------------------------------------------------
+
+    def collections (self, limit=10, offset=None, order=None,
+                     order_direction=None, institution=None,
+                     published_since=None, modified_since=None, group=None,
+                     resource_doi=None, doi=None, handle=None, account_id=None,
+                     search_for=None, id=None):
+
+        if order_direction is None:
+            order_direction = "DESC"
+        if limit is None:
+            limit = 10
+        if order is None:
+            order="?id"
+        else:
+            order = f"?{order}"
+
+        query = f"""\
+{self.default_prefixes}
+SELECT DISTINCT ?account_id
+                ?resource_id
+                ?resource_doi
+                ?resource_title
+                ?resource_link
+                ?resource_version
+                ?version
+                ?description
+                ?institution_id
+                ?group_id
+                ?articles_count
+                ?is_public
+                ?citation
+                ?group_resource_id
+                ?custom_fields_id
+                ?modified_date
+                ?created_date
+                ?timeline_posted
+                ?timeline_publisher_acceptance
+                ?timeline_publisher_publication
+                ?timeline_first_online
+                ?timeline_revision
+                ?timeline_submission
+                ?id
+                ?title
+                ?doi
+                ?handle
+                ?url
+                ?published_date
+WHERE {{
+  GRAPH <{self.state_graph}> {{
+    ?collection            rdf:type                 sg:Collection .
+    ?collection            col:id                   ?id .
+    ?collection            col:timeline_id          ?timeline_id .
+
+    OPTIONAL {{
+        ?timeline           rdf:type                 sg:Timeline .
+        ?timeline           col:id                   ?timeline_id .
+
+        OPTIONAL {{ ?timeline col:firstonline          ?timeline_first_online . }}
+        OPTIONAL {{ ?timeline col:publisheracceptance  ?timeline_publisher_acceptance . }}
+        OPTIONAL {{ ?timeline col:publisherpublication ?timeline_publisher_publication . }}
+        OPTIONAL {{ ?timeline col:submission           ?timeline_submission . }}
+        OPTIONAL {{ ?timeline col:posted               ?timeline_posted . }}
+        OPTIONAL {{ ?timeline col:revision             ?timeline_revision . }}
+    }}
+
+    OPTIONAL {{ ?collection col:account_id         ?account_id . }}
+    OPTIONAL {{ ?collection col:resource_id        ?resource_id . }}
+    OPTIONAL {{ ?collection col:resource_doi       ?resource_doi . }}
+    OPTIONAL {{ ?collection col:resource_title     ?resource_title . }}
+    OPTIONAL {{ ?collection col:resource_link      ?resource_link . }}
+    OPTIONAL {{ ?collection col:resource_version   ?resource_version . }}
+    OPTIONAL {{ ?collection col:version            ?version . }}
+    OPTIONAL {{ ?collection col:description        ?description . }}
+    OPTIONAL {{ ?collection col:institution_id     ?institution_id . }}
+    OPTIONAL {{ ?collection col:group_id           ?group_id . }}
+    OPTIONAL {{ ?collection col:articles_count     ?articles_count . }}
+    OPTIONAL {{ ?collection col:is_public          ?is_public . }}
+    OPTIONAL {{ ?collection col:citation           ?citation . }}
+    OPTIONAL {{ ?collection col:group_resource_id  ?group_resource_id . }}
+    OPTIONAL {{ ?collection col:custom_fields_id   ?custom_fields_id . }}
+    OPTIONAL {{ ?collection col:modified_date      ?modified_date . }}
+    OPTIONAL {{ ?collection col:created_date       ?created_date . }}
+    OPTIONAL {{ ?collection col:title              ?title . }}
+    OPTIONAL {{ ?collection col:doi                ?doi . }}
+    OPTIONAL {{ ?collection col:handle             ?handle . }}
+    OPTIONAL {{ ?collection col:url                ?url . }}
+    OPTIONAL {{ ?collection col:published_date     ?published_date . }}
+  }}
+"""
+
+        if institution is not None:
+            query += f"FILTER (?institution_id={institution})\n"
+
+        if published_since is not None:
+            query += "FILTER (BOUND(?published_date))\n"
+            query += "FILTER (STR(?published_date) != \"NULL\")\n"
+            query += f"FILTER (STR(?published_date) > \"{published_since}\")\n"
+
+        if modified_since is not None:
+            query += "FILTER (BOUND(?modified_date))\n"
+            query += "FILTER (STR(?modified_date) != \"NULL\")\n"
+            query += f"FILTER (STR(?modified_date) > \"{modified_since}\")\n"
+
+        if group is not None:
+            query += f"FILTER (?group_id = {group})\n"
+
+        if resource_doi is not None:
+            query += f"FILTER (STR(?resource_doi) = \"{resource_doi}\")\n"
+
+        if doi is not None:
+            query += f"FILTER (STR(?doi) = \"{doi}\")\n"
+
+        if handle is not None:
+            query += f"FILTER (STR(?handle) = \"{handle}\")\n"
+
+        if id is not None:
+            query += f"FILTER (?id = {id})\n"
+
+        if account_id is None:
+            query += "FILTER (?is_public = 1)\n"
+        else:
+            query += f"FILTER (?account_id = {account_id})\n"
+
+        if search_for is not None:
+            query += f"FILTER(CONTAINS(?title, \"{search_for}\"))\n"
+            query += f"FILTER(CONTAINS(?resource_title, \"{search_for}\"))\n"
+            query += f"FILTER(CONTAINS(?description, \"{search_for}\"))\n"
+            query += f"FILTER(CONTAINS(?citation, \"{search_for}\"))\n"
+
+        query += "}\n"
+
+        if order is not None:
+            query += f"""\
+ORDER BY {order_direction}({order})
+LIMIT {limit}
+"""
+
+        self.sparql.setQuery(query)
+        results = []
+        try:
+            query_results = self.sparql.query().convert()
+            results = list(map(self.normalize_binding, query_results["results"]["bindings"]))
+        except:
+            logging.error(f"SPARQL query failed.")
+            logging.error(f"Query:\n---\n{query}\n---")
+
+        return results
