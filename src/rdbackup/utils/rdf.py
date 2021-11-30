ed3359c158f51742fbda2704440fd4daae7e803f
Roel Janssen <r.r.e.janssen@tudelft.nl>
2021-11-30 10:23:16 +0100

    utils: Introduce “rdf” module.
    This module contains convenience methods and constants to use while
    handling RDF throughout the codebase.
    
    * src/rdbackup/utils/rdf.py: New file.

---
+++ src/rdbackup/utils/rdf.py
@@ -0,0 +1,14 @@
+"""
+This module provides convenience functions for handling RDF.
+"""
+
+from rdflib import Graph, Literal, Namespace
+
+def add (graph, subject, predicate, value):
+    """Adds the triplet SUBJECT PREDICATE VALUE if VALUE is set."""
+    if value is not None:
+        graph.add(graph, subject, predicate, Literal(value))
+
+ROW = Namespace("origin://rdbackup#")
+SG  = Namespace("https://sparqling-genomics.org/0.99.12/")
+COL = Namespace("sg://0.99.12/table2rdf/Column/")
