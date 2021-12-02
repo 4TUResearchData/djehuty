2d3cddb8e60b0ae5a5027d0a19c62b4620d0edb8
Roel Janssen <r.r.e.janssen@tudelft.nl>
2021-12-02 14:13:48 +0100

    utils: rdf: Add convenience procedures to assemble SPARQL queries.
    * src/djehuty/utils/rdf.py: Add ‘sparql_filter’ and ‘sparql_suffix’.

--- src/djehuty/utils/rdf.py
+++ src/djehuty/utils/rdf.py
@@ -9,6 +9,33 @@ def add (graph, subject, predicate, value):
     if value is not None:
         graph.add((subject, predicate, Literal(value)))
 
+
+def sparql_filter (name, value, escape=False):
+    query   = ""
+    literal = f"\"{value}\"" if escape else value
+    if value is not None:
+        query += f"FILTER (?{name}={literal})\n"
+
+    return query
+
+def sparql_suffix (order, order_direction, limit=None, offset=None):
+    if order_direction is None:
+        order_direction = "DESC"
+    else:
+        order_direction = order_direction.upper()
+
+    order = "?id" if order is not None else f"?{order}"
+
+    query = f"ORDER BY {order_direction}({order})"
+
+    if limit is not None:
+        query += f"\nLIMIT {limit}"
+
+    if offset is not None:
+        query += f"\nOFFSET {offset}"
+
+    return query
+
 ROW = Namespace("origin://djehuty#")
 SG  = Namespace("https://sparqling-genomics.org/0.99.12/")
 COL = Namespace("sg://0.99.12/table2rdf/Column/")
