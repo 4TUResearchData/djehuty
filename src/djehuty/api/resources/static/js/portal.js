d91eea9cf2e60f42804a8179f655aac83bdc60e1
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-01-12 09:05:55 +0100

    api: portal: Apply more style tweaks.
    * src/djehuty/api/resources/static/css/main.css: Make the pages more
      compact in width.
    * src/djehuty/api/resources/static/js/portal.js: Add colors to the
      “latest datasets”.

--- src/djehuty/api/resources/static/js/portal.js
+++ src/djehuty/api/resources/static/js/portal.js
@@ -1,19 +1,35 @@
 jQuery(document).ready(function() {
     var jqxhr = jQuery.get("https://api.figshare.com/v2/articles", {
-        "limit":           20,
+        "limit":           30,
         "order_direction": "desc",
         "order":           "published_date",
         "institution":     898
     }, function() {
     })
         .done(function(data) {
-            output = "<ul>";
+            output = '<ul class="latest-datasets">';
+            num_items = 0;
             jQuery.each (data, function(index) {
-                output += '<li><a href="'+ data[index].url_public_html +'">';
+                if (num_items < 3) {
+                    output += '<li class="datasets-stage datasets-stage-one">';
+                } else if (num_items < 8) {
+                    output += '<li class="datasets-stage datasets-stage-two">';
+                } else if (num_items < 13) {
+                    output += '<li class="datasets-stage datasets-stage-three">';
+                } else if (num_items < 22) {
+                    output += '<li class="datasets-stage datasets-stage-four">';
+                } else {
+                    output += '<li class="datasets-stage datasets-stage-five">';
+                }
+
+                output += '<a href="'+ data[index].url_public_html +'">';
                 output += data[index].title + '</a></li>';
+
+                num_items += 1;
             });
-            jQuery("#latest-datasets").append(output);
+
             output += "</ul>";
+            jQuery("#latest-datasets").append(output);
         })
         .fail(function() {
             jQuery("#latest-datasets").append("<p>Could not load the latest datasets.</p>");
