b1b09d9fa63e9b1f1db5780dcf3ea1f98b94d66d
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-04-08 14:11:34 +0200

    utils: constants: Remove trailing whitespace.
    * src/djehuty/utils/constants.py: Remove trailing whitespace.

--- src/djehuty/utils/constants.py
+++ src/djehuty/utils/constants.py
@@ -14,5 +14,3 @@ member_url_names = { 'delft'     : 'Delft_University_of_Technology',
                      'twente'    : 'University_of_Twente',
                      'wageningen': 'Wageningen_University_and_Research',
                      'other'     : 'Other_institutions' }
-    
-                    
