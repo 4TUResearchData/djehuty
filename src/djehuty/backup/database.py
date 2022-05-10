c5ff0df32895eff662e0a85d8b24ac010efe664c
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-05-10 09:05:21 +0200

    backup: database: Fix bug in storing CustomField type.
    It was stored as a literal string instead of a URI.
    
    * src/djehuty/backup/database.py: Store CustomField type as URI.

--- src/djehuty/backup/database.py
+++ src/djehuty/backup/database.py
@@ -342,7 +342,7 @@ class DatabaseInterface:
         field_uri = next (subjects, None)
         if field_uri is None:
             field_uri = rdf.ROW[f"custom_field_{name}"]
-            rdf.add (self.store, field_uri, RDF.type,                rdf.SG["CustomField"])
+            self.store.add ((field_uri,     RDF.type,                rdf.SG["CustomField"]))
             rdf.add (self.store, field_uri, rdf.COL["name"],         name,                                        XSD.string)
             rdf.add (self.store, field_uri, rdf.COL["original_name"], field["name"],                              XSD.string)
             rdf.add (self.store, field_uri, rdf.COL["max_length"],   value_or_none (validations, "max_length"))
