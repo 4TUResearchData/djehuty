e1fe659f83930388b2c0809d67be67f760a90e47
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-06-13 11:24:53 +0200

    web: database: Remove unused ‘insert_author_link’ and related.
    * src/djehuty/web/database.py: Remove ‘insert_author_link’,
      ‘insert_article_author’, ‘insert_collection_author’.

--- src/djehuty/web/database.py
+++ src/djehuty/web/database.py
@@ -1143,34 +1143,6 @@ class SparqlInterface:
         """Procedure to add a link between a collection and a category."""
         return self.insert_item_category (collection_version_id, category_id, "collection")
 
-    def insert_author_link (self, author_id, item_id, item_type="article"):
-        """Procedure to add a link to an author."""
-
-        graph = Graph()
-
-        prefix = item_type.capitalize()
-        link_id  = self.ids.next_id(f"{item_type}_author")
-        link_uri = rdf.ROW[f"{item_type}_author_link_{link_id}"]
-
-        graph.add ((link_uri, RDF.type,                   rdf.SG[f"{prefix}Author"]))
-        graph.add ((link_uri, rdf.COL["id"],              Literal(link_id)))
-        graph.add ((link_uri, rdf.COL["author_id"],       Literal(author_id)))
-        graph.add ((link_uri, rdf.COL[f"{item_type}_version_id"], Literal(item_id)))
-
-        query = self.__insert_query_for_graph (graph)
-        if self.__run_query(query):
-            return link_id
-
-        return None
-
-    def insert_article_author (self, article_id, author_id):
-        """Procedure to add a link between an article and an author."""
-        return self.insert_author_link (author_id, article_id, item_type="article")
-
-    def insert_collection_author (self, collection_version_id, author_id):
-        """Procedure to add a link between a collection and a author."""
-        return self.insert_author_link (author_id, collection_version_id, item_type="collection")
-
     def insert_article_file (self, article_version_id, file_id):
         """Procedure to add a link between an article and a file."""
 
