78cfe2b9c147ae08244f1aa263af61cd62b66e07
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-06-21 16:30:44 +0200

    web: wsgi: Refactor ‘api_my_collections’.
    * src/djehuty/web/wsgi.py: Do error-handling immediately to avoid nesting.

--- src/djehuty/web/wsgi.py
+++ src/djehuty/web/wsgi.py
@@ -925,29 +925,27 @@ class ApiServer:
         return self.error_500 ()
 
     def api_my_collections (self, request):
-        if self.accepts_html (request):
-            account_id = self.account_id_from_request (request)
-            if account_id is None:
-                return self.error_authorization_failed(request)
+        if not self.accepts_html (request):
+            return self.error_406 ("text/html")
 
-            token = self.token_from_cookie (request)
-            if self.db.is_depositor (token):
-                collections = self.db.collections (account_id   = account_id,
-                                                   is_published = False,
-                                                   limit        = 10000)
+        account_id = self.account_id_from_request (request)
+        if account_id is None:
+            return self.error_authorization_failed(request)
 
-                for collection in collections:
-                    count = self.db.collections_article_count(collection["uri"])
-                    collection["number_of_articles"] = count
-
-                return self.__render_template (request, "depositor/my-collections.html",
-                                               collections = collections)
-
+        token = self.token_from_cookie (request)
+        if not self.db.is_depositor (token):
             return self.error_404 (request)
 
-        return self.response (json.dumps({
-            "message": "This page is meant for humans only."
-        }))
+        collections = self.db.collections (account_id   = account_id,
+                                           is_published = False,
+                                           limit        = 10000)
+
+        for collection in collections:
+            count = self.db.collections_article_count(collection["uri"])
+            collection["number_of_articles"] = count
+
+        return self.__render_template (request, "depositor/my-collections.html",
+                                       collections = collections)
 
     def api_edit_collection (self, request, collection_id):
         if self.accepts_html (request):
