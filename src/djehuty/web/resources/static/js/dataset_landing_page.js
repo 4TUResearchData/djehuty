2431cbbc5045f803e0d638c6d00925310c2b6f8e
Egbert Gramsbergen <e.f.gramsbergen@tudelft.nl>
2022-09-27 13:59:08 +0200

    web: dataset landing page: add functionality to add dataset to collection
    * src/djehuty/web/resources/static/js/dataset_landing_page.js: new file
    * src/djehuty/web/resources/html_templates/dataset.html:
      add js link and styles
    * src/djehuty/web/resources/html_templates/public_metadata.html:
      add buttons and list of collections
    * src/Makefile.am: add new file to listing

---
+++ src/djehuty/web/resources/static/js/dataset_landing_page.js
@@ -0,0 +1,15 @@
+function add_dataset_to_collection (dataset_id, collection_id) {
+    jQuery.ajax({
+        url:         `/v2/account/collections/${collection_id}/articles`,
+        type:        "POST",
+        contentType: "application/json",
+        accept:      "application/json",
+        data:        JSON.stringify({ "articles": [dataset_id] }),
+    }).done(function () {
+        window.alert('Dataset succesfully added to collection');
+        document.getElementById("add_to_collection").style.display="none"
+    }).fail(function () {
+        console.log (`Failed to add ${dataset_id}`);
+        window.alert('Failed to add dataset to collection');
+    });
+}
