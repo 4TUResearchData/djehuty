30ce70ed6c2f2af5c4f1398c7c98319972000ea2
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-11-21 21:30:20 +0100

    web: js: utils: Implement ‘install_sticky_header’.
    * src/djehuty/web/resources/static/js/utils.js: Implement
      ‘install_sticky_header’ and ‘install_touchable_help_icons’.
    * src/djehuty/web/resources/static/js/edit-collection.js: Use the new
      utility functions.
    * src/djehuty/web/resources/static/js/edit-dataset.js: Likewise.

--- src/djehuty/web/resources/static/js/edit-dataset.js
+++ src/djehuty/web/resources/static/js/edit-dataset.js
@@ -597,29 +597,9 @@ function toggle_access_level () {
 function activate (dataset_uuid) {
     var submenu_offset = jQuery("#submenu").offset().top;
 
-    jQuery(window).on('resize scroll', function() {
-        let scroll_offset  = jQuery(window).scrollTop();
-        if (submenu_offset <= scroll_offset) {
-            jQuery("#submenu").addClass("sticky");
-            jQuery("#message").addClass("sticky-message");
-            jQuery("h1").addClass("sticky-margin");
-            jQuery("#message").width(jQuery("#content-wrapper").width());
-        } else {
-            jQuery("#submenu").removeClass("sticky");
-            jQuery("#message").removeClass("sticky-message");
-            jQuery("h1").removeClass("sticky-margin");
-        }
-    });
+    install_sticky_header();
+    install_touchable_help_icons();
 
-    jQuery(".help-icon").on("click", function () {
-        let selector = jQuery(this).find(".help-text");
-        if (selector.is(":visible") ||
-            selector.css("display") != "none") {
-            jQuery(this).removeClass("help-icon-clicked");
-        } else {
-            jQuery(this).addClass("help-icon-clicked");
-        }
-    });
     jQuery.ajax({
         url:         `/v2/account/articles/${dataset_uuid}`,
         type:        "GET",
--- src/djehuty/web/resources/static/js/utils.js
+++ src/djehuty/web/resources/static/js/utils.js
@@ -14,3 +14,31 @@ function show_message (type, message) {
     }, 5000);
 }
 
+function install_sticky_header () {
+    var submenu_offset = jQuery("#submenu").offset().top;
+    jQuery(window).on('resize scroll', function() {
+        let scroll_offset  = jQuery(window).scrollTop();
+        if (submenu_offset <= scroll_offset) {
+            jQuery("#submenu").addClass("sticky");
+            jQuery("#message").addClass("sticky-message");
+            jQuery("h1").addClass("sticky-margin");
+            jQuery("#message").width(jQuery("#content-wrapper").width());
+        } else {
+            jQuery("#submenu").removeClass("sticky");
+            jQuery("#message").removeClass("sticky-message");
+            jQuery("h1").removeClass("sticky-margin");
+        }
+    });
+}
+
+function install_touchable_help_icons () {
+    jQuery(".help-icon").on("click", function () {
+        let selector = jQuery(this).find(".help-text");
+        if (selector.is(":visible") ||
+            selector.css("display") != "none") {
+            jQuery(this).removeClass("help-icon-clicked");
+        } else {
+            jQuery(this).addClass("help-icon-clicked");
+        }
+    });
+}
