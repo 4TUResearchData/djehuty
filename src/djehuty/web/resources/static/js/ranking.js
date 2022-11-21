426fbabf7eeea037759e18b5ed2a803112304393
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-11-21 11:19:04 +0100

    web: js: Refactor/merge “ranking.js” and “ranking_by_institution.js”.
    * src/Makefile.am: Remove “ranking_by_institution.js”.
    * src/djehuty/web/resources/html_templates/institutions.html: Import
      “ranking.js” instead of “ranking_by_institution.js”.
    * src/djehuty/web/resources/html_templates/search.html: Remove
      reference to “ranking_by_institution.js”.
    * src/djehuty/web/resources/static/js/ranking.js: Refactor so that it
      can be used for both the institutional pages as well as the category
      pages.
    * src/djehuty/web/resources/static/js/ranking_by_institution.js:
      Remove file.

--- src/djehuty/web/resources/static/js/ranking.js
+++ src/djehuty/web/resources/static/js/ranking.js
@@ -1,47 +1,58 @@
-const cumulativeSum = (sum => value => sum += value)(0); // https://stackoverflow.com/a/55261098
-const capitalize    = (i) => (i[0].toUpperCase() + i.substring(1));
+const capitalize = (i) => (i[0].toUpperCase() + i.substring(1))
 
-function latest_datasets () {
-    parameters = {
-        limit:           10,
-        order_direction: "desc",
-        order:           "published_date",
+function parameters_for_api_calls (order) {
+    let parameters = {
+        "limit": 10,
+        "order_direction": "desc",
+        "order": order
     }
 
-    if (categories !== "") { jQuery.extend(parameters, { "categories": categories }) }
+    // This procedure is used by both institutional pages which filter by 'group_ids'
+    // and category pages which filter by 'categories'.  So one of these variables
+    // won't be defined, but that's expected.
+    try {
+        if (categories !== "") { jQuery.extend(parameters, { "categories": categories }); }
+    } catch (error) {}
 
-    var jqxhr = jQuery.get("/v3/datasets", parameters, function() {
-    })
-        .done(function(data) {
-            output = '<ul class="latest-datasets">';
-            num_items = 0;
-            jQuery.each (data, function(index) {
-                if (num_items < 3) {
-                    output += '<li class="datasets-stage datasets-stage-one">';
-                } else if (num_items < 8) {
-                    output += '<li class="datasets-stage datasets-stage-two">';
-                } else if (num_items < 13) {
-                    output += '<li class="datasets-stage datasets-stage-three">';
-                } else if (num_items < 22) {
-                    output += '<li class="datasets-stage datasets-stage-four">';
-                } else {
-                    output += '<li class="datasets-stage datasets-stage-five">';
-                }
+    try {
+        if (group_ids !== "") { jQuery.extend(parameters, { "group_ids": group_ids }); }
+    } catch (error) {}
 
-                output += '<a href="/datasets/'+ data[index].id +'">';
-                output += data[index].title + '</a></li>';
+    return parameters;
+}
 
-                num_items += 1;
-            });
+function latest_datasets () {
+    const parameters = parameters_for_api_calls ("published_date");
+    jQuery.get("/v3/datasets", parameters, function() {
+    }).done(function(data) {
+        let output = '<ul class="latest-datasets">';
+        let num_items = 0;
+        jQuery.each (data, function(index) {
+            if (num_items < 3) {
+                output += '<li class="datasets-stage datasets-stage-one">';
+            } else if (num_items < 8) {
+                output += '<li class="datasets-stage datasets-stage-two">';
+            } else if (num_items < 13) {
+                output += '<li class="datasets-stage datasets-stage-three">';
+            } else if (num_items < 22) {
+                output += '<li class="datasets-stage datasets-stage-four">';
+            } else {
+                output += '<li class="datasets-stage datasets-stage-five">';
+            }
 
-            output += "</ul>";
-            jQuery("#latest-datasets-loader").hide();
-            jQuery("#latest-datasets").append(output);
-        })
-        .fail(function() {
-            jQuery("#latest-datasets-loader").hide();
-            jQuery("#latest-datasets").append("<p>Could not load the latest datasets.</p>");
+            output += '<a href="/datasets/'+ data[index].id +'">';
+            output += data[index].title + '</a></li>';
+
+            num_items += 1;
         });
+
+        output += "</ul>";
+        jQuery("#latest-datasets-loader").hide();
+        jQuery("#latest-datasets").append(output);
+    }).fail(function() {
+        jQuery("#latest-datasets-loader").hide();
+        jQuery("#latest-datasets").append("<p>Could not load the latest datasets.</p>");
+    });
 }
 
 function top_datasets (item_type) {
@@ -49,33 +60,28 @@ function top_datasets (item_type) {
     jQuery("#top-datasets tbody tr").css('opacity', '0.15');
     jQuery("#top-buttons .active").removeClass("active");
     jQuery(".top-" + item_type).addClass("active");
-    var jqxhr = jQuery.get("/v3/datasets/top/" + item_type, {
-        "limit":           10,
-        "order_direction": "desc",
-        "order":           item_type,
-        "categories":      categories
-    }, function() {
-    })
-        .done(function(data) {
-            var output = '<table id="top-datasets"><thead>';
-            output += '<tr><th>Dataset</th><th># '+ capitalize(item_type) +'</th></tr>';
-            output += '</thead><tbody>';
-            jQuery.each (data, function(index) {
-                output += '<tr><td>';
-                output += '<a href="/datasets/'+ data[index].container_uuid +'">';
-                output += data[index].title + '</a>';
-                output += '</td>';
-                output += '<td>' + data[index][item_type] + '</td></tr>';
-            });
 
-            output += "</tbody></table>";
-            jQuery("#top-datasets").remove()
-            jQuery("#top-datasets-wrapper").removeClass("loader");
-            jQuery("#top-datasets-wrapper").append(output);
-        })
-        .fail(function() {
-            jQuery("#top-datasets-wrapper").append("<p>Could not load the top datasets.</p>");
+    const parameters = parameters_for_api_calls (item_type);
+    jQuery.get("/v3/datasets/top/" + item_type, parameters, function() {
+    }).done(function(data) {
+        let output = '<table id="top-datasets"><thead>';
+        output += '<tr><th>Dataset</th><th># '+ capitalize(item_type) +'</th></tr>';
+        output += '</thead><tbody>';
+        jQuery.each (data, function(index) {
+            output += '<tr><td>';
+            output += '<a href="/datasets/'+ data[index].container_uuid +'">';
+            output += data[index].title + '</a>';
+            output += '</td>';
+            output += '<td>' + data[index][item_type] + '</td></tr>';
         });
+
+        output += "</tbody></table>";
+        jQuery("#top-datasets").remove()
+        jQuery("#top-datasets-wrapper").removeClass("loader");
+        jQuery("#top-datasets-wrapper").append(output);
+    }).fail(function() {
+        jQuery("#top-datasets-wrapper").append("<p>Could not load the top datasets.</p>");
+    });
 }
 
 jQuery(document).ready(function() {
