94745b5d22385a5b89f4b62a17790bbd54dc753c
Roel Janssen <r.r.e.janssen@tudelft.nl>
2022-11-24 09:52:31 +0100

    tests: Revise “run_backup”.
    * tests/run_backup.py: Apply linter suggestions.

--- tests/run_backup.py
+++ tests/run_backup.py
@@ -5,24 +5,21 @@ from a running instance.
 
 import os
 import unittest
-import requests
-import json
-import djehuty.utils.convenience as conv
 import djehuty.backup.ui as backup_ui
 
-account_id = os.getenv("DJEHUTY_ACCOUNT_ID")
-token      = os.getenv("DJEHUTY_TEST_TOKEN")
-base_url   = "http://127.0.0.1:8080/v2"
+ACCOUNT_ID = os.getenv("DJEHUTY_ACCOUNT_ID")
+TOKEN      = os.getenv("DJEHUTY_TEST_TOKEN")
+BASE_URL   = "http://127.0.0.1:8080/v2"
 
 class TestRunBackup(unittest.TestCase):
+    """Class to run the backup tool in itself."""
 
     def __init__(self, *args, **kwargs):
         super(TestRunBackup, self).__init__(*args, **kwargs)
-        self.collection_location = None
 
     ## Create and destroy
     ## ------------------------------------------------------------------------
     def test_run_backup (self):
         """Runs the back-up tool and tests whether it reaches the end."""
-        result = backup_ui.main (token, "-", account_id, base_url)
+        result = backup_ui.main (TOKEN, "-", ACCOUNT_ID, BASE_URL)
         self.assertTrue (result)
