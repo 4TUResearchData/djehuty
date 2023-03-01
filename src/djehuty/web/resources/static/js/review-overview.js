bbafa02938631ac8dc027392dc17edc252671c7a
Roel Janssen <r.r.e.janssen@tudelft.nl>
2023-03-01 20:15:24 +0100

    web: js: review-overview: Fix filter for reviewer.
    * src/djehuty/web/resources/static/js/review-overview.js: Fix filter
      for reviewer.

--- src/djehuty/web/resources/static/js/review-overview.js
+++ src/djehuty/web/resources/static/js/review-overview.js
@@ -16,14 +16,23 @@ function assign_reviewer (event) {
 
 function filter_reviewer (event) {
     let value = jQuery(".reviewer-filter option:selected").val();
+    let name  = jQuery(".reviewer-filter option:selected").text();
     jQuery('#overview-table tr').each(function(index, element) {
+        let reviewer_element = jQuery(element).find(`td .reviewer-selector option:selected`);
+        let status = jQuery(element).find(`td:nth-child(5)`).text();
         if (jQuery(element).find("th").length > 0) {} // Skip the header.
         else if (value == "all") { jQuery(element).show(); }
-        else if (value == "unassigned" && jQuery(element).find(`td .reviewer-selector option:selected`).val() == "") {
+        else if (value == "unassigned" && reviewer_element.length > 0 && reviewer_element.val() == "") {
             jQuery(element).show();
         }
-        else if (jQuery(element).find(`td .reviewer-selector option:selected`).val().split(":").pop() == value) {
+        else if (reviewer_element.length > 0 && reviewer_element.val().split(":").pop() == value) {
             jQuery(element).show();
+        }
+        else if (status == "accepted") {
+            let reviewer = jQuery(element).find(`td:nth-child(9)`).text();
+            let reviewer_name = reviewer.split("\n").map(function (item){ return item.trim(); }).join(" ").trim();
+            if (reviewer_name == name) { jQuery(element).show(); }
+            else { jQuery(element).hide(); }
         }
         else {
             jQuery(element).hide();
