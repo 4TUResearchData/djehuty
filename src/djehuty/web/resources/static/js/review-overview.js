802d9014f56f3e5ee31bf7ed9c6709ccd5258f1e
Roel Janssen <r.r.e.janssen@tudelft.nl>
2023-03-01 16:47:29 +0100

    web: html_templates: review overview: Enable searching, sorting and filtering.
    * src/djehuty/web/resources/html_templates/review/overview.html:
      Enable DataTable; Add modified date.
    * src/djehuty/web/resources/sparql_templates/reviews.sparql: Add
      modified date.
    * src/djehuty/web/resources/static/js/review-overview.js: Implement
      filter functionality for/on reviewers.

--- src/djehuty/web/resources/static/js/review-overview.js
+++ src/djehuty/web/resources/static/js/review-overview.js
@@ -14,6 +14,24 @@ function assign_reviewer (event) {
     });
 }
 
+function filter_reviewer (event) {
+    let value = jQuery(".reviewer-filter option:selected").val();
+    jQuery('#overview-table tr').each(function(index, element) {
+        if (jQuery(element).find("th").length > 0) {} // Skip the header.
+        else if (value == "all") { jQuery(element).show(); }
+        else if (value == "unassigned" && jQuery(element).find(`td .reviewer-selector option:selected`).val() == "") {
+            jQuery(element).show();
+        }
+        else if (jQuery(element).find(`td .reviewer-selector option:selected`).val().split(":").pop() == value) {
+            jQuery(element).show();
+        }
+        else {
+            jQuery(element).hide();
+        }
+    });
+}
+
 function activate () {
     jQuery(".reviewer-selector").change(assign_reviewer);
+    jQuery(".reviewer-filter").change(filter_reviewer);
 }
