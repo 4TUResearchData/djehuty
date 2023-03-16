deab6e8650c512e1ec1d2d1f850690244d80f9d0
Roel Janssen <r.r.e.janssen@tudelft.nl>
2023-03-16 19:24:35 +0100

    web: js: review-overview: Fix filters.
    * src/djehuty/web/resources/static/js/review-overview.js: Fix filters.

--- src/djehuty/web/resources/static/js/review-overview.js
+++ src/djehuty/web/resources/static/js/review-overview.js
@@ -1,3 +1,7 @@
+function cleanup_name(name) {
+    return name.split("\n").map(function (item){ return item.trim(); }).join(" ").trim();
+}
+
 function assign_reviewer (event) {
 
     let event_local = this;
@@ -19,40 +23,40 @@ function assign_reviewer (event) {
     });
 }
 
+function apply_filters (event) {
+    jQuery('#overview-table tr').each(function(index, element) {
+        jQuery(element).show();
+    });
+    filter_reviewer (event);
+    filter_status (event);
+}
+
 function filter_reviewer (event) {
     let value = jQuery(".reviewer-filter option:selected").val();
-    let name  = jQuery(".reviewer-filter option:selected").text();
+    let name  = cleanup_name(jQuery(".reviewer-filter option:selected").text());
     jQuery('#overview-table tr').each(function(index, element) {
         let reviewer_element = jQuery(element).find(`td .reviewer-selector option:selected`);
-        let status = jQuery(element).find(`td:nth-child(5)`).text();
+        let status = jQuery(element).find(`td:nth-child(6)`).text().trim();
         if (jQuery(element).find("th").length > 0) {} // Skip the header.
-        else if (value == "all") { jQuery(element).show(); }
-        else if (value == "unassigned" && reviewer_element.length > 0 && reviewer_element.val() == "") {
-            jQuery(element).show();
-        }
-        else if (reviewer_element.length > 0 && reviewer_element.val().split(":").pop() == value) {
-            jQuery(element).show();
-        }
+        else if (value == "all") {}
+        else if (value == "unassigned" && reviewer_element.length > 0 && reviewer_element.val() == "") {}
+        else if (reviewer_element.length > 0 && reviewer_element.val().split(":").pop() == value) {}
         else if (status == "approved") {
-            let reviewer = jQuery(element).find(`td:nth-child(9)`).text();
-            let reviewer_name = reviewer.split("\n").map(function (item){ return item.trim(); }).join(" ").trim();
-            if (reviewer_name == name) { jQuery(element).show(); }
-            else { jQuery(element).hide(); }
+            let reviewer = jQuery(element).find(`td:nth-child(10)`).text()
+            let reviewer_name = cleanup_name(reviewer);
+            if (reviewer_name != name) { jQuery(element).hide(); }
         }
-        else {
-            jQuery(element).hide();
-        }
+        else { jQuery(element).hide(); }
     });
 }
 
 function filter_status (event) {
     let value = jQuery(".status-filter option:selected").val();
     jQuery('#overview-table tr').each(function(index, element) {
-        let reviewer_element = jQuery(element).find(`td .reviewer-selector option:selected`);
-        let status = jQuery(element).find(`td:nth-child(6)`).text();
+        let status = jQuery(element).find(`td:nth-child(6)`).text().trim();
         if (jQuery(element).find("th").length > 0) {} // Skip the header.
-        else if (value == "all") { jQuery(element).show(); }
-        else if (value == status) { jQuery(element).show(); }
+        else if (value == "all") { /*jQuery(element).show();*/ }
+        else if (value == status) { /*jQuery(element).show();*/ }
         else { jQuery(element).hide(); }
     });
 }
@@ -73,8 +77,8 @@ function activate() {
         });
 
         jQuery(".reviewer-selector").change(assign_reviewer);
-        jQuery(".reviewer-filter").change(filter_reviewer);
-        jQuery(".status-filter").change(filter_status);
+        jQuery(".reviewer-filter").change(apply_filters);
+        jQuery(".status-filter").change(apply_filters);
         jQuery("#content-wrapper").show();
     });
 }
