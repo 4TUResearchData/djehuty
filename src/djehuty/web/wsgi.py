d907f416d1437069b1df4ba703fcd41c7a998d70
Roel Janssen <r.r.e.janssen@tudelft.nl>
2023-03-20 08:47:29 +0100

    web: wsgi: Bail early in ‘ui_search’ when no search parameter is provided.
    * src/djehuty/web/wsgi.py: Add check for a missing “search_for”.

--- src/djehuty/web/wsgi.py
+++ src/djehuty/web/wsgi.py
@@ -2862,6 +2862,10 @@ class ApiServer:
         """Implements /search."""
         if self.accepts_html (request):
             search_for = self.get_parameter(request, "search")
+            if search_for is None:
+                return self.__render_template (request, "search.html", search_for="",
+                                               articles=[], dataset_count=0, display_terms=[])
+
             search_for = search_for.strip()
             operators_mapping = {"(":"(", ")":")", "AND":"&&", "OR":"||"}
             operators = operators_mapping.keys()
