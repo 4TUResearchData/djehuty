05d8d040885f715760405c54bc481492e094f065
Roel Janssen <r.r.e.janssen@tudelft.nl>
2023-04-26 10:39:53 +0200

    web: js: dataset_landing_page: Avoid erroneous event handling.
    * src/djehuty/web/resources/static/js/dataset_landing_page.js: Avoid
      erroneous event handling in ‘toggle_access_request’ when manually
      invoking ‘toggle_access_request’.

--- src/djehuty/web/resources/static/js/dataset_landing_page.js
+++ src/djehuty/web/resources/static/js/dataset_landing_page.js
@@ -15,8 +15,10 @@ function add_dataset_to_collection (dataset_id, collection_id) {
 }
 
 function toggle_access_request (event) {
-    event.preventDefault();
-    event.stopPropagation();
+    if (event !== null) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
     let access_request_div = jQuery("#access-request-wrapper");
     if (access_request_div.is(":visible")) {
         jQuery("#access-request-wrapper").slideUp(150, function (){
