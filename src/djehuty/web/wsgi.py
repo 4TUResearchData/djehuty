5d238bdc15eeb7117a60624b86a2040f4cd6f9e8
Roel Janssen <r.r.e.janssen@tudelft.nl>
2023-12-07 10:09:18 +0100

    web: wsgi: Implement lock-free removal of a file from a dataset.
    Similar to 9b964bdac0bec770592cbd44ecb872a64de8b09f, this commit
    implements lock-free removal of a file from a dataset.
    
    * src/djehuty/web/wsgi.py: Use ‘delete_item_from_list’ to remove a
      file from the file list rather than re-inserting the entire file
      list minus the file-to-be-removed.

--- src/djehuty/web/wsgi.py
+++ src/djehuty/web/wsgi.py
@@ -4556,28 +4556,21 @@ class ApiServer:
                 if dataset is None:
                     return self.error_403 (request)
 
-                self.locks.lock (locks.LockTypes.FILE_LIST)
-                files = self.db.dataset_files (dataset_uri  = dataset["uri"],
-                                               account_uuid = account_uuid)
-                files.remove (next (filter (lambda item: item["uuid"] == file_id, files)))
-                files = list(map (lambda item: URIRef(uuid_to_uri(item["uuid"], "file")),
-                                           files))
+                metadata = self.__file_by_id_or_uri (file_id,
+                                                     account_uuid = account_uuid,
+                                                     dataset_uri = dataset["uri"])
 
-                if self.db.update_item_list (dataset["uuid"],
-                                             account_uuid,
-                                             files,
-                                             "files"):
-
-                    self.locks.unlock (locks.LockTypes.FILE_LIST)
+                if self.db.delete_item_from_list (dataset["uri"], "files",
+                                                  uuid_to_uri (metadata["uuid"], "file")):
                     self.db.cache.invalidate_by_prefix (f"{account_uuid}_storage")
                     self.db.cache.invalidate_by_prefix (f"{dataset['uuid']}_dataset_storage")
                     return self.respond_204()
+                else:
+                    self.log.error ("Failed to delete file %s from dataset %s.",
+                                    file_id, dataset_id)
 
             except (IndexError, KeyError, StopIteration):
                 pass
-
-            self.locks.unlock (locks.LockTypes.FILE_LIST)
-            return self.error_500()
 
         return self.error_500 ()
 
