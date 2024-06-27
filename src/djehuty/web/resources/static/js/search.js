f68da44749deb735fc0dd11cb9ce9f14cdfa42bd
Roel Janssen <r.r.e.janssen@tudelft.nl>
2024-06-27 07:57:38 +0200

    web: js: search: Fix displaying results in Chrome and Edge.
    * src/djehuty/web/resources/static/js/search.js: Account for text
      after the RGB specification in the regular expression in
      ‘get_corporate_background_color’.

--- src/djehuty/web/resources/static/js/search.js
+++ src/djehuty/web/resources/static/js/search.js
@@ -740,5 +740,5 @@ function get_corporate_background_color() {
     if (rgb === undefined) {
         return "#000000";
     }
-    return `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`;
+    return `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\).*$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`;
 }
