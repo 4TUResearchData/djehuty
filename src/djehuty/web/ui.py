4TUResearchData/djehuty

commit cd691dc567d45b08a183f5b752cbf5fea861319f

web: ui: Reduce command-line configurable arguments.
In practice, configuring all options in the configuration file is a
better idea.  So this commit removes the under-used command-line
arguments that have equivalents in the configuration file.

* src/djehuty/ui.py: Adapt help message and remove command-line
  arguments for the 'web' sub-command.
* src/djehuty/web/ui.py: Simplify parameters of
  ‘read_configuration_file’ and ‘main’.

src/djehuty/web/ui.py
@@ -521,9 +521,7 @@ def read_email_configuration (server, xml_root, logger):
             logger.error ("Could not configure the email subsystem:")
             logger.error ("The email port should be a numeric value.")
 
-def read_configuration_file (server, config_file, address, port, state_graph,
-                             storage, base_url, use_debugger, use_reloader,
-                             logger, config_files):
+def read_configuration_file (server, config_file, logger, config_files):
     """Procedure to parse a configuration file."""
 
     inside_reload = os.environ.get('WERKZEUG_RUN_MAIN')
@@ -550,14 +548,14 @@ def read_configuration_file (server, config_file, address, port, state_graph,
             config["log-file"] = log_file
             configure_file_logging (log_file, inside_reload, logger)
 
-        config["address"]       = config_value (xml_root, "bind-address", address, "127.0.0.1")
-        config["port"]          = int(config_value (xml_root, "port", port, 8080))
-        server.base_url         = config_value (xml_root, "base-url", base_url,
+        config["address"]       = config_value (xml_root, "bind-address", None, "127.0.0.1")
+        config["port"]          = int(config_value (xml_root, "port", None, 8080))
+        server.base_url         = config_value (xml_root, "base-url", None,
                                                 f"http://{config['address']}:{config['port']}")
-        server.db.storage       = config_value (xml_root, "storage-root", storage)
-        server.db.state_graph   = config_value (xml_root, "rdf-store/state-graph", state_graph)
-        config["use_reloader"]  = config_value (xml_root, "live-reload", use_reloader)
-        config["use_debugger"]  = config_value (xml_root, "debug-mode", use_debugger)
+        server.db.storage       = config_value (xml_root, "storage-root")
+        server.db.state_graph   = config_value (xml_root, "rdf-store/state-graph")
+        config["use_reloader"]  = config_value (xml_root, "live-reload")
+        config["use_debugger"]  = config_value (xml_root, "debug-mode")
         config["maximum_workers"] = int(config_value (xml_root, "maximum-workers", None, 1))
 
         endpoint = config_value (xml_root, "rdf-store/sparql-uri")
@@ -726,17 +724,7 @@ def read_configuration_file (server, config_file, address, port, state_graph,
             if not os.path.isabs(include):
                 include = os.path.join(config_dir, include)
 
-            new_config = read_configuration_file (server,
-                                                  include,
-                                                  config["address"],
-                                                  config["port"],
-                                                  server.db.state_graph,
-                                                  server.db.storage,
-                                                  server.base_url,
-                                                  config["use_debugger"],
-                                                  config["use_reloader"],
-                                                  logger,
-                                                  config_files)
+            new_config = read_configuration_file (server, include, logger, config_files)
             config = { **config, **new_config }
 
         read_menu_configuration (xml_root, server)
@@ -899,9 +887,7 @@ def apply_transactions_from_directory (logger, server, config, transactions_dire
 ## Starting point for the command-line program
 ## ----------------------------------------------------------------------------
 
-def main (address=None, port=None, state_graph=None, storage=None,
-          base_url=None, config_file=None, use_debugger=False,
-          use_reloader=False, run_internal_server=True, initialize=True,
+def main (config_file=None, run_internal_server=True, initialize=True,
           extract_transactions_from_log=None, apply_transactions=None):
     """The main entry point for the 'web' subcommand."""
     try:
@@ -928,10 +914,7 @@ def main (address=None, port=None, state_graph=None, storage=None,
 
         server = wsgi.ApiServer ()
         config_files = set()
-        config = read_configuration_file (server, config_file, address, port,
-                                          state_graph, storage, base_url,
-                                          use_debugger, use_reloader, logger,
-                                          config_files)
+        config = read_configuration_file (server, config_file, logger, config_files)
 
         ## Handle extracting Query Audit Logs early on.
         if extract_transactions_from_log is not None:

src/djehuty/ui.py
@@ -35,14 +35,7 @@ Available subcommands and options:
 
   web:
     --help               -h Show a help message.
-    --port=ARG           -p The port to start the web server on.
-    --address=ARG        -a The address to bind the server on.
-    --state-graph        -s The state graph in the RDF store.
-    --storage            -S The storage path backing the API.
-    --base-url=ARG       -b URL on which this instance is accessed by users.
     --config-file=ARG    -c Load configuration from a file.
-    --debug              -d Enable debugging.
-    --dev-reload         -r Enable active reloading.
     --initialize         -i Populate the RDF store with default triples.
     --extract-transactions-from-log=ARG
                          -e Extract transactions from the log file.
@@ -94,14 +87,7 @@ def main_inner ():
     ### WEB SUBCOMMAND
     ### -----------------------------------------------------------------------
     web_parser = subparsers.add_parser('web', help="Options for the 'web' subcommand.")
-    web_parser.add_argument('--address',    '-a', type=str, default=None)
-    web_parser.add_argument('--port',       '-p', type=int, default=None)
-    web_parser.add_argument('--state-graph','-s', type=str, default=None)
-    web_parser.add_argument('--storage',    '-S', type=str, default=None)
-    web_parser.add_argument('--base-url',   '-b', type=str, default=None)
     web_parser.add_argument('--config-file','-c', type=str, default=None)
-    web_parser.add_argument('--debug',      '-d', action='store_true')
-    web_parser.add_argument('--dev-reload', '-r', action='store_true')
     web_parser.add_argument('--initialize', '-i', action='store_true')
     web_parser.add_argument('--extract-transactions-from-log', '-e', nargs='?',
                             const='', default=None)
@@ -137,9 +123,7 @@ def main_inner ():
                             args.api_url)
 
     if args.command == "web":
-        web_ui.main (args.address, args.port, args.state_graph, args.storage,
-                     args.base_url, args.config_file, args.debug,
-                     args.dev_reload, True, args.initialize,
+        web_ui.main (args.config_file, True, args.initialize,
                      args.extract_transactions_from_log,
                      args.apply_transactions)
 

commit 81c6ba3b0f55d2fdf68b5f44ffd33b17d4d6fe95

web: ui: Properly read/restore the address and port configuration values.
This problem was introduced in cd691dc567d45b08a183f5b752cbf5fea861319f.

* src/djehuty/web/ui.py (read_configuration_file): Restore configured
  values for address and port.

src/djehuty/web/ui.py
@@ -548,8 +548,10 @@ def read_configuration_file (server, config_file, logger, config_files):
             config["log-file"] = log_file
             configure_file_logging (log_file, inside_reload, logger)
 
-        config["address"]       = config_value (xml_root, "bind-address", None, "127.0.0.1")
-        config["port"]          = int(config_value (xml_root, "port", None, 8080))
+        address                 = value_or_none (config, "address")
+        port                    = value_or_none (config, "port")
+        config["address"]       = config_value (xml_root, "bind-address", address, "127.0.0.1")
+        config["port"]          = int(config_value (xml_root, "port", port, 8080))
         server.base_url         = config_value (xml_root, "base-url", None,
                                                 f"http://{config['address']}:{config['port']}")
         server.db.storage       = config_value (xml_root, "storage-root")