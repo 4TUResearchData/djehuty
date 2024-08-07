25234967cf989f3d47378212cd10ef840c40a360
Roel Janssen <r.r.e.janssen@tudelft.nl>
2024-08-07 10:19:03 +0200

    web: ui: Fix configuration file loading amnesia.
    This commit fixes the amnesia introduced in cd691dc5.
    
    * src/djehuty/web/ui.py: Keep original values for “storage-root”,
      “state-graph”, “live-reload” and “debug-mode”.

--- src/djehuty/web/ui.py
+++ src/djehuty/web/ui.py
@@ -548,16 +548,21 @@ def read_configuration_file (server, config_file, logger, config_files):
             config["log-file"] = log_file
             configure_file_logging (log_file, inside_reload, logger)
 
-        address                 = value_or_none (config, "address")
-        port                    = value_or_none (config, "port")
+        address                 = convenience.value_or_none (config, "address")
+        port                    = convenience.value_or_none (config, "port")
         config["address"]       = config_value (xml_root, "bind-address", address, "127.0.0.1")
         config["port"]          = int(config_value (xml_root, "port", port, 8080))
         server.base_url         = config_value (xml_root, "base-url", None,
                                                 f"http://{config['address']}:{config['port']}")
-        server.db.storage       = config_value (xml_root, "storage-root")
-        server.db.state_graph   = config_value (xml_root, "rdf-store/state-graph")
-        config["use_reloader"]  = config_value (xml_root, "live-reload")
-        config["use_debugger"]  = config_value (xml_root, "debug-mode")
+
+        server.db.storage       = config_value (xml_root, "storage-root", None, server.db.storage)
+        server.db.state_graph   = config_value (xml_root, "rdf-store/state-graph", None, server.db.state_graph)
+
+        live_reload             = convenience.value_or_none (config, "live-reload")
+        config["use_reloader"]  = config_value (xml_root, "live-reload", None, live_reload)
+
+        debug_mode              = convenience.value_or_none (config, "debug-mode")
+        config["use_debugger"]  = config_value (xml_root, "debug-mode", None, debug_mode)
         config["maximum_workers"] = int(config_value (xml_root, "maximum-workers", None, 1))
 
         endpoint = config_value (xml_root, "rdf-store/sparql-uri")
