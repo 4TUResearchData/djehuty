42e5fc965c3825bd0330aecd195d42d972a78fd2
Roel Janssen <r.r.e.janssen@tudelft.nl>
2024-09-17 08:21:24 +0200

    web: wsgi: Resolve linter suggestion to use f-string in ‘ui_home’.
    * src/djehuty/web/wsgi.py: Format the summary numbers using an
      f-string in ‘ui_home’.

--- src/djehuty/web/wsgi.py
+++ src/djehuty/web/wsgi.py
@@ -3153,7 +3153,7 @@ class ApiServer:
         summary_data = self.db.repository_statistics()
         try:
             for key in summary_data:
-                summary_data[key] = "{:,}".format(int(summary_data[key]))  # pylint: disable=consider-using-f-string
+                summary_data[key] = f"{int(summary_data[key]):,}"
         except ValueError:
             summary_data = { "datasets": 0, "authors": 0, "collections": 0, "files": 0, "bytes": 0 }
 
