40ca0660bdbce81fe1d91c45d7c0c8d7bdb68a4f
Gabriela Kuhn <g.kuhn@tudelft.nl>
2026-06-17 15:50:59 +0200

    web: Add Created and Updated physical sample date types
    * src/djehuty/web/resources/html_templates/depositor/edit-physical-sample.html:
      Update the Dates help text.
    * src/djehuty/web/resources/static/js/edit-physical-sample.js: Add Created
      and Updated options to the date type dropdown.
    * src/djehuty/web/wsgi.py: Accept Created and Updated as valid date types.
    * src/djehuty/backup/database.py: Add labels for the Created and Updated
      physical sample date types.
    * src/djehuty/web/xml_formatter.py: Map Created and Updated date types to
      DataCite format.

--- src/djehuty/web/wsgi.py
+++ src/djehuty/web/wsgi.py
@@ -3576,7 +3576,7 @@ class WebServer:
                 return self.error_500 ()
 
             record = request.get_json()
-            types  = ["collected", "destroyed", "issued", "other"]
+            types  = ["collected", "created", "destroyed", "issued", "updated", "other"]
             errors = []
 
             if not isinstance (record, list):
