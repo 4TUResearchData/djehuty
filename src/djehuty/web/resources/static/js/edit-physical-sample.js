ec44c4533daa64d411497d0945d0399bb6f6bc59
Gabriela Kuhn <g.kuhn@tudelft.nl>
2026-07-31 18:06:10 +0200

    web: Issued date as published date on physical sample.
    * src/djehuty/web/resources/static/js/edit-physical-sample.js: Remove the
      Issued option from the date type options so it cannot be entered by the user.
    * src/djehuty/web/wsgi.py: Drop issued from the accepted date types and
      record the issued date as the publication date in the
      database on the first publication of a physical sample.
    * src/djehuty/web/xml_formatter.py: Skip the persisted issued date when
      building the DataCite tree so it is not written twice.

--- src/djehuty/web/resources/static/js/edit-physical-sample.js
+++ src/djehuty/web/resources/static/js/edit-physical-sample.js
@@ -469,7 +469,6 @@ function render_dates (container_uuid) {
         row += '<option value="" disabled="disabled" selected="selected">Date type</option>';
         row += '<option value="collected">Collected</option>';
         row += '<option value="destroyed">Destroyed</option>';
-        row += '<option value="issued">Issued</option>';
         row += '<option value="other">Other</option>';
         row += '</select></td>';
         row += '<td><a class="form-button corporate-identity-standard-button add-date-button" href="#">Add</a></td></tr>';
